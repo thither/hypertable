f7407cefd9c806d91051b2289f8f26d9836f6bda
Kashirin Alex <kashirin.alex@gmail.com>
2018-06-01 14:21:36 +0300

    cmake tests adjustments ThriftClientZ require 1st arg zlib

--- tests/integration/thrift-reconnect-hyperspace/run.sh
+++ tests/integration/thrift-reconnect-hyperspace/run.sh
@@ -2,7 +2,7 @@
 
 HT_TEST_DFS=${HT_TEST_DFS:-local}
 if [ ! -z $HT_TEST_THRIFT_TP ]; then
-	HT_TEST_THRIFT_TP=--thrift-transport=${HT_TEST_THRIFT_TP}
+	transport=--thrift-transport=${HT_TEST_THRIFT_TP}
 fi
 set -v
 
@@ -10,7 +10,7 @@ HT_HOME=${INSTALL_DIR:-"$HOME/hypertable/current"}
 
 $HT_HOME/bin/ht-start-test-servers.sh --clear
 $HT_HOME/bin/ht-stop-thriftbroker.sh
-$HT_HOME/bin/ht-start-thriftbroker.sh ${HT_TEST_THRIFT_TP}
+$HT_HOME/bin/ht-start-thriftbroker.sh ${transport}
 
 sleep 5;
 $HT_HOME/bin/ht-stop-servers.sh --no-thriftbroker 
@@ -19,4 +19,4 @@ $HT_HOME/bin/ht-start-all-servers.sh --no-thriftbroker $HT_TEST_DFS
 sleep 12;
 
 cd ${THRIFT_CPP_TEST_DIR};
-./${TEST_BIN}
+./${TEST_BIN} ${HT_TEST_THRIFT_TP}
